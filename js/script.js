edab0d1d1d009a4cd09d2e366d170c8cfe27de57
balakovskiybro <balakovskiybro@gmail.com>
2024-10-02 16:05:37 +0300

    initial-commit

--- js/script.js
+++ js/script.js
@@ -6,3 +6,18 @@ play.addEventListener('click', () => {
   video.setAttribute('controls', 'controls');
   play.classList.add('about__play--hidden');
 })
+
+function customScrollFunction(id, top) {
+  document.getElementById(id).addEventListener('click', () => {
+    window.scrollTo({
+      top,
+      left: 0,
+      behavior: "smooth",
+    })
+  })
+}
+
+customScrollFunction("portfolio", 900)
+customScrollFunction("team", 2562)
+customScrollFunction("blog", 3700)
+customScrollFunction("contacts", 6000)
